bd57d89edb60c62e7641f4c9a9febb1eeccde7ad
Nathan Getachew <nathangetachew95@gmail.com>
2023-04-15 21:40:13 +0300

    setup

---
+++ Hacking with iOS SwiftUI/Hacking_with_iOS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacking_with_iOS_SwiftUIApp.swift
+//  Hacking with iOS SwiftUI
+//
+//  Created by Nathan Getachew on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Hacking_with_iOS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
